a98ef849c3158db16c503fd7e5dec39eeec73fd8
Maxwell Tietze <max.tietze@15five.com>
2020-10-21 19:25:29 -0400

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- test_file.py
+++ test_file.py
@@ -1,8 +1,5 @@
 import argparse
-
 import requests
-from collections import defaultdict
-import git
 
 
 def print_hi(name):
@@ -10,8 +7,11 @@ def print_hi(name):
 
 
 if __name__ == "__main__":
-    print_hi('PyCharm')
-    a = {1:2,2:3,4:5,5:6,'a':"121212234234ewfdfgwrgsdfwerghsfgnsfgsfbsdfgadfvbadfvasdfsDFsdffdvzsdfszdfvsdfvasdfsdfsDFsdfsDFsdf"}
-
-
-
+    print_hi("PyCharm")
+    a = {
+        1: 2,
+        2: 3,
+        4: 5,
+        5: 6,
+        "a": "121212234234ewfdfgwrgsdfwerghsfgnsfgsfbsdfgadfvbadfvasdfsDFsdffdvzsdfszdfvsdfvasdfsdfsDFsdfsDFsdf",
+    }
