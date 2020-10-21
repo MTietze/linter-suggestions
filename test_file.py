b7a55f02fb1bc43579a95b7ae70dd1da2148f46f
Max Tietze <max.tietze@15five.com>
2020-10-21 19:02:13 -0400

    bad file

--- test_file.py
+++ test_file.py
@@ -1,23 +1,18 @@
-import argparse
+from io import StringIO
+import requests
 
 import git
-import requests
+import argparse
 
 
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f"Hi, {name}")  # Press ⌘F8 to toggle the breakpoint.
+    print(f"Hi, {name}")
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == "__main__":
-    print_hi("PyCharm")
-    a = {
-        1: 2,
-        2: 3,
-        4: 5,
-        5: 6,
-        "a": "121212234234ewfdfgwrgsdfwerghsfgnsfgsfbsdfgadfvbadfvasdfsDFsdffdvzsdfszdfvsdfvasdfsdfsDFsdfsDFsdf",
-    }
+    print_hi('PyCharm')
+    a = {1:2,2:3,4:5,5:6,'a':"121212234234ewfdfgwrgsdfwerghsfgnsfgsfbsdfgadfvbadfvasdfsDFsdffdvzsdfszdfvsdfvasdfsdfsDFsdfsDFsdf"}
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
